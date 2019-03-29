5ff592d16a3b5ec2e736d9afa78e4105f0acf00f
“Andrew <andrewwoods88@gmail.com>
2019-03-29 11:30:56 -0400

    regex work

--- objects/regex/test-method.js
+++ objects/regex/test-method.js
@@ -25,3 +25,62 @@ let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result3 = petRegex.test(petString);
 console.log(result3);
+
+// ignore case with /i
+let myString1 = "freeCodeCamp";
+let fccRegex = /FreeCodeCamp/i; // Change this line
+let result4 = fccRegex.test(myString1);
+console.log(result4);
+
+
+// using match
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result6 = extractStr.match(codingRegex); // Change this line
+console.log(result6);
+
+
+// To search or extract a pattern more than once, you can use the g flag.
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result5 = twinkleStar.match(starRegex); // Change this line
+console.log(result5);
+
+
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+humStr.match(huRegex); // Returns ["hum"]
+hugStr.match(huRegex); // Returns ["hug"]
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex); // Returns ["big"]
+bagStr.match(bgRegex); // Returns ["bag"]
+bugStr.match(bgRegex); // Returns ["bug"]
+bogStr.match(bgRegex); // Returns null
+
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result7 = quoteSample.match(vowelRegex);
+console.log(result7);
+
+
+//  using a character range
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bg1Regex = /[a-e]at/;
+catStr.match(bg1Regex); // Returns ["cat"]
+batStr.match(bg1Regex); // Returns ["bat"]
+matStr.match(bg1Regex); // Returns null
+
+
+let quoteSample2 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]gi/; // Change this line
+let result8 = alphabetRegex; 
---
+++ objects/state/cart.js
@@ -0,0 +1,90 @@
+
+import React, { Component } from 'react';
+
+console.log("farts");
+
+class ProductProvider extends Component {
+
+  state = {
+      products: [],
+      detailProduct: detailProduct,
+      cart: []
+    };
+
+    componentDidMount(){
+      this.setProducts();
+    }
+
+    setProducts = () => {
+
+      let tempProducts = [];
+
+      storeProducts.forEach(item => {
+        const singleItem = {...item};
+        console.log(singleItem);
+        
+        tempProducts = [...tempProducts, singleItem];  
+      })
+
+      this.setState(() => {
+        return {products: tempProducts}
+      })
+    }
+
+    getItem = (id) => {
+      const product = this.state.products.find(item => item.id === id );
+      console.log('getItem product: ', product);
+      return product;
+    }
+
+    handleDetail = (id) => {
+      console.log("handleDetail ran");  
+      const product = this.getItem(id);
+      this.setState(() => {
+        return {
+          // set individual product to detailProduct in state
+          detailProduct: product
+        }
+      })
+    };
+
+    addToCart = (id) => {
+
+      let tempProducts = [...this.state.products];
+      const index= tempProducts.indexOf(this.getItem(id));
+      console.log(index);
+      const product = tempProducts[index];
+      product.inCart = true;
+      product.count = 1;
+      const price = product.price;
+      product.total = price;
+
+      this.setState(() => {
+        return {
+            products: tempProducts,
+            cart: [...this.state.cart, product]
+          }
+      },
+      () => {console.log('cart: ', this.state);
+      }
+      );
+    };
+
+    render() {
+      console.log('trying to check state:', this.state.products);
+      return (
+         value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}>
+        {/* <button onClick={this.tester}>test me</button> */}
+
+          
+        
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
